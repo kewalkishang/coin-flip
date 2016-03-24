7cb352b5333da8df32ff2a5d03657100cf039521
kewalkishang <kewalkishang@gmail.com>
2016-03-24 21:43:40 +0530

    first commit

---
+++ Assets/scripts/rotate.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var un : int;
+var asl;
+function Start () {
+Screen.orientation = ScreenOrientation.Portrait;
+un=0;
+asl=Random.Range(15,25);
+}
+
+function Update () {
+if(toss1.ing==1)
+{transform.Rotate(0,0,asl);
+un=1;
+}
+if(un==1)
+{ //if((transform.rotation.eulerAngles.z<88&&transform.rotation.eulerAngles.z>178)||(transform.rotation.eulerAngles.z<358&&transform.rotation.eulerAngles.z>88)||(transform.rotation.eulerAngles.z<178&&transform.rotation.eulerAngles.z>268)||(transform.rotation.eulerAngles.z<268&&transform.rotation.eulerAngles.z>350))
+  if((transform.rotation.eulerAngles.z>2&&transform.rotation.eulerAngles.z<178)||(transform.rotation.eulerAngles.z>182&&transform.rotation.eulerAngles.z<358))
+  transform.Rotate(0,0,2);
+}
+}
---
+++ Assets/scripts/toss1.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+static var ing : int;
+var goa : int;
+var col : Collider;
+var rb: Rigidbody;
+function Start () {
+Screen.orientation = ScreenOrientation.Portrait;
+ing =0;
+goa =2;
+}
+
+function Update () {
+ if (Input.GetKeyDown(KeyCode.Escape)) 
+    Application.Quit(); 
+
+if(goa==0)
+{col.transform.Translate(Vector3.up*Time.deltaTime*2);
+if(transform.position.y>7)
+goa=1;
+}
+else
+if(goa==1)
+{col.attachedRigidbody.useGravity=true;
+if(transform.position.y<4.1){
+col.attachedRigidbody.useGravity=false;
+ing=0;
+}
+}
+}
+
+
+function  FixedUpdate(){
+if(Input.GetTouch(0).phase==TouchPhase.Began)
+{ing=1;
+goa=0;
+}
+}
+
